25e3fb308ad280fe2b291dcb01a0e02585f511dd
Chomp <9366621-chomp9@users.noreply.gitlab.com>
2021-12-03 00:21:15 -0700

    updates ant build.xml and convenience cmd scripts
    * create ant tasks: compile-bot, compile-scripts, compile-all, run-bot, clean
    * create compile_all.cmd
    * create compile_scripts.cmd
    * create run.bat
    * create run_noconsole.bat

--- run.cmd
+++ run.cmd
@@ -1,4 +1,10 @@
 @echo off
-SET antpath="apache-ant-1.10.5\bin\"
 
-%antpath%ant -f build.xml runclient
+IF NOT EXIST bot.jar (
+    echo Missing bot.jar ...
+    echo.
+    call compile_all.cmd
+)
+
+start javaw -jar bot.jar %1
+exit
