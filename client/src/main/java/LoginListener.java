64296b9fc7d2ba8a4c8e8cac9ed695f0df1701ed
Kaila_Btw <sudoer@live.com>
2023-03-30 19:33:02 -0400

    More LoginListener changes
    Sets lowest time to 35 seconds, after 10 attempts goes up to ~2 mins minimum, increasing to 4 mins quickly, and 6-7 mins after long periods.  Fixed the multiple next_attempts per login Attempt, feature is functioning correctly now.

--- client/src/main/java/LoginListener.java
+++ client/src/main/java/LoginListener.java
@@ -58,21 +58,23 @@ public final class LoginListener implements ILoginListener {
 	public void onLoginResponse(final String arg0, final String arg1) {
 		long loginDelay;
 		long quickLoginDelay;
-		System.out.printf("%s %s%n", arg0, arg1);
-		System.out.println("Current Login Attempts: " + loginCount);
-		 if (loginCount <= 10) { //wait 25-45s for autolog the first 10 times (i.e. for ~ 2-3 mins)
-			quickLoginDelay = (long) (Math.random() * 10000L);
-			next_attempt = System.currentTimeMillis() + 25000L + quickLoginDelay;
-			System.out.println("Waiting " + ((quickLoginDelay + 25000L)/1000L) + " seconds for the next autologin attempt.");
-		} else if (loginCount > 10) {
-			int i = loginCount - 9;
-			loginDelay = (long) (Math.random() * 30000L)
-				+ (((i * 15L) / (i + 60L)) * 15000L)
-				+ 35000L; //calculate our delay. Approaches 4.6 mins at very high "i" values. Average wait value at i = 9 is about 1 minute
-			next_attempt = System.currentTimeMillis() + loginDelay;
-			System.out.println("Waiting " + (loginDelay/1000L) + " seconds for the next autologin attempt.");
+		if(System.currentTimeMillis() >= next_attempt) {
+			loginCount++;
+			System.out.printf("%s %s%n", arg0, arg1);
+			System.out.println("Current Login Attempts: " + loginCount);
+			if (loginCount <= 10) { //wait 35-45s for autolog the first 10 times (i.e. for ~ 2-3 mins)
+				quickLoginDelay = (long) (Math.random() * 10000L);
+				next_attempt = System.currentTimeMillis() + 35000L + quickLoginDelay;
+				System.out.println("Waiting " + ((quickLoginDelay + 25000L) / 1000L) + " seconds for the next autologin attempt.");
+			} else {
+				int i = loginCount - 6;
+				loginDelay = (long) (Math.random() * 60000L)
+					+ (((i * 15L) / (i + 60L)) * 10000L)
+					+ 90000L; //calculate our delay. Approaches 4.6 mins at very high "i" values. Average wait is about 3-5 minute
+				next_attempt = System.currentTimeMillis() + loginDelay;
+				System.out.println("Waiting " + (loginDelay / 1000L) + " seconds for the next autologin attempt.");
+			}
 		}
-		loginCount++;
 	}
 	public static LoginListener getInstance() {
 		return instance;
