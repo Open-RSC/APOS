7be49312bdc8378f4cb3a8bbf9d9dba4b7ad364a
Millz <>
2023-12-08 10:24:15 +0000

    Changed window title to be toggled via in-game command. Changed arg separator to =

--- client/src/main/java/CommandListener.java
+++ client/src/main/java/CommandListener.java
@@ -1,4 +1,5 @@
 import com.aposbot._default.IClient;
+import com.aposbot.gui.BotFrame;
 
 /**
  * Listens for ::commands typed in-game and toggles client features.
@@ -9,6 +10,7 @@ final class CommandListener {
 	static boolean showCombatStyleMenu;
 	static boolean hideCombatStyleMenu;
 	static boolean showReportPlayer;
+	static boolean showWindowDebug;
 
 	private CommandListener() {
 	}
@@ -28,6 +30,12 @@ final class CommandListener {
 			case "menu":
 				showCombatStyleMenu = !showCombatStyleMenu;
 				client.displayMessage("@cya@menu: " + showCombatStyleMenu);
+				return false;
+			case "window":
+				BotFrame.showWindowDebug = !BotFrame.showWindowDebug;
+				client.displayMessage(BotFrame.showWindowDebug
+					? "@cya@window: move or resize window to display debug values"
+					: "@cya@window: hidden");
 				return false;
 			default:
 				return ScriptListener.getInstance().onCommand(command);
