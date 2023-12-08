a861946092f98edaed6ce1f7ce37983533d75588
Millz <>
2023-12-08 10:25:45 +0000

    removed comments from run_autologin.cmd, details in the readme

--- run_autologin.cmd
+++ run_autologin.cmd
@@ -1,13 +1,3 @@
-REM ------------------------------------------------
-REM Command-line args:
-REM --no-console		Launches the bot without console
-REM --username:username	Default account name for launcher. Must already be added as account.
-REM --height:558		Also: --h. Window height. Height and Width must both be set together.
-REM --width:670			Also: --w. Window width. Height and Width must both be set together.
-REM --x:200				X Location on screen. X and Y must both be set together.
-REM --y:200				Y Location on screen. X and Y must both be set together.
-REM ------------------------------------------------
-
 @echo off
 
 IF NOT EXIST bot.jar (
