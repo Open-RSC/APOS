fb5f8205dcbe809ab3bcd9b834dfa9e7a77381c9
Prince <prince.mst13@itbhu.ac.in>
2023-12-30 19:39:38 +0530

    maybe found main game object class using reflection

--- client/src/main/java/Test_RSCE.java
+++ client/src/main/java/Test_RSCE.java
@@ -2,7 +2,12 @@ import com.rsc.client.RSCFrame;
 
 import javax.swing.*;
 import java.awt.*;
+
+import com.rsc.e;
+
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class Test_RSCE {
 
@@ -13,7 +18,7 @@ public class Test_RSCE {
 		var2.getContentPane().setLayout(new BorderLayout());
 		var2.setDefaultCloseOperation(3);
 		var2.setIconImage(Toolkit.getDefaultToolkit().getImage(com.rsc.a.a + File.separator + "RuneScape.png"));
-		var2.getContentPane().add(getDemoUI().add(var1));
+		var2.getContentPane().add(var1);
 		var2.setResizable(true);
 		var2.setVisible(true);
 		var2.setBackground(Color.black);
@@ -21,10 +26,71 @@ public class Test_RSCE {
 		var2.pack();
 		var2.setLocationRelativeTo((Component)null);
 		var1.init();
-		var1.start();
+		//var1.start();
+		e player = getClass_a_e_static_var();
+		String player_vars = get_class_a_e_variables_as_string(player);
+		System.out.println(player_vars);
+	}
+	public static e getClass_a_e_static_var()
+	{
+		try
+		{
+			Class<?> forName = Class.forName("com.rsc.client.a");
+			Field[] declaredFields = forName.getDeclaredFields();
+			/*for(Field field: declaredFields)
+			{
+				String name = field.getName();
+				System.out.println(name);
+				Class<?> type = field.getType();
+				System.out.println(type);
+				field.setAccessible(true);
+				Object object = field.get(type);
+				System.out.println(object);
+			}*/
+			Field field = declaredFields[0];
+			//System.out.println(field.getName());
+			System.out.println(field.getType());
+			field.setAccessible(true);
+			e result = (e) field.get(field.getType());
+			System.out.println(result);
+			return result;
+
+		}
+		catch (Exception exp)
+		{
+			exp.printStackTrace();
+		}
+		return null;
 	}
 
-	public static JPanel getDemoUI(){
+    static String get_class_a_e_variables_as_string(e obj) {
+		try
+		{
+			StringBuffer buffer = new StringBuffer();
+			Field[] fields = obj.getClass().getDeclaredFields();
+			for (Field f : fields) {
+				if (!Modifier.isStatic(f.getModifiers())) {
+					f.setAccessible(true);
+					Object value = f.get(obj);
+					buffer.append(f.getType().getName());
+					buffer.append(" ");
+					buffer.append(f.getName());
+					buffer.append("=");
+					buffer.append("" + value);
+					buffer.append("\n");
+				}
+			}
+			return buffer.toString();
+
+		}
+		catch (Exception exp)
+		{
+			exp.printStackTrace();
+		}
+		return null;
+	}
+
+    public static JPanel getDemoUI(){
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout());
 		JLabel label = new JLabel("JFrame By Example");
