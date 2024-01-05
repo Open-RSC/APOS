fd81814ea0d2e82dab4f1b501726672807fd056c
Prince <prince.mst13@itbhu.ac.in>
2024-01-05 19:31:05 +0530

    Update gvy_test_rsce.groovy

--- client/src/main/java/gvy_test_rsce.groovy
+++ client/src/main/java/gvy_test_rsce.groovy
@@ -2,16 +2,24 @@ import static Util.*;
 
 static void Main(Object arg) {
     try {
+        System.out.print("Done...");
+        System.out.println("Execution Started..");
+        System.out.println("-----------------------------------");
         arg;
+        System.out.println("-----------------------------------");
+
     } catch (Exception e) {
         e.printStackTrace();
         System.out.println();
     }
 }
 
+Main(test())
+
 void test() {
     def player = ((Test_RSCE)var1).player
-    Gprint(player.t);
+    //println(player.u);
+    Test_RSCE.walk_to_coords(player, 300, 400)
 }
 
-Main(test())
+
